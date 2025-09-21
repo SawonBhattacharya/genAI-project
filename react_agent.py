d798c046fe84458436269c8cce4cb936b996d5f1
SawonBhattacharya <sawon17081997@gmail.com>
2025-09-21 18:56:01 +0000

    Basic Files added for GenAI Project

--- react_agent.py
+++ react_agent.py
@@ -18,8 +18,8 @@ MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
 MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
 MYSQL_DB = os.getenv("MYSQL_DB", "rpsg_rag")
 MYSQL_USER = os.getenv("MYSQL_USER", "rpsg_user")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD","Your DB Password")
-GROQ_API_KEY = os.getenv("GROQ_API_KEY","Your GROQ Password")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD") #("Your DB Password")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY") #("Your GROQ API Key")
 
 def get_connection():
     """Create and return a SQLAlchemy engine for MySQL."""
